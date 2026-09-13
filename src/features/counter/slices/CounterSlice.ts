f21856383c52769441b547f51f7462cb6d121733
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-13 21:07:17 +0900

    feat: Implement Counter Slice

---
+++ src/features/counter/slices/CounterSlice.ts
@@ -0,0 +1,24 @@
+import type { CounterType } from "../types/CounterType";
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState: CounterType = { count: 0 };
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1;
+    },
+    decrement: (state) => {
+      state.count -= 1;
+    },
+    reset: (state) => {
+      state.count = 0;
+    },
+  },
+});
+
+export const { increment, decrement, reset } = counterSlice.actions;
+
+export default counterSlice.reducer;
