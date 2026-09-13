a68da548a82bfafc89695bd667a0c15594ed69cd
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-13 22:06:46 +0900

    feat: Implement ThemeToggle Component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Provider } from "react-redux";
 import "./App.css";
 import CounterComponent from "./features/counter/components/CounterComponent";
 import store from "./app/store";
+import ThemeToggle from "./features/theme/components/ThemeToggle";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div>
         <Provider store={store}>
           <CounterComponent />
+          <ThemeToggle />
         </Provider>
       </div>
     </>
